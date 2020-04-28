b7a90b0500bc3fd77d759615075dcb8bed81b153
Zerrick Staples <zerrickstaples@Zerricks-MacBook-Pro.local>
2020-04-28 15:23:28 -0400

    Set up function to scrape website, set up variables for siteUrl and search query terms

--- Program.cs
+++ Program.cs
@@ -18,5 +18,24 @@ namespace WebScraper
         {
             
         }
+
+        private string Title { get; set; }
+        private string Url { get; set; }
+        private string siteUrl = "https://www.oceannetworks.ca/news/stories";
+        public string[] QueryTerms { get; } = { "Ocean", "Nature", "Pollution" };
+
+        internal async void ScrapeWebsite()
+        {
+            CancellationTokenSource cancellationToken = new CancellationTokenSource();
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage request = await httpClient.GetAsync(siteUrl);
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            Stream response = await request.Content.ReadAsStreamAsync();
+            cancellationToken.Token.ThrowIfCancellationRequested();
+
+            HtmlParser parser = new HtmlParser();
+            IHtmlDocument document = parser.ParseDocument(response);
+        }
     }
 }
