566fbc1614d5c206d48a0467fe544fcc1372112c
Zerrick Staples <zerrickstaples@Zerricks-MacBook-Pro.local>
2020-04-28 12:10:14 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+using AngleSharp.Html.Parser;
+using AngleSharp.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Windows;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
